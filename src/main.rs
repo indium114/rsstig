ab38e0327529db347335d55bdddde3f9294e684a
indium114 <156162907+indium114@users.noreply.github.com>
2026-09-04 15:08:53 +0200

    feat: parse opml

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use color_eyre::eyre::Result;
+
+mod opml;
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    println!("{:#?}", opml::load_feeds());
+
+    Ok(())
 }
---
+++ src/opml.rs
@@ -0,0 +1,15 @@
+use opml::OPML;
+use std::fs;
+
+fn home() -> String {
+    let dir = dirs::home_dir();
+    return dir
+        .map(|p| p.to_string_lossy().into_owned())
+        .unwrap_or_default();
+}
+
+pub fn load_feeds() -> OPML {
+    let dir = home() + "/.config/rsstig/feeds.opml";
+    let file = fs::read_to_string(&dir).expect(&format!("Failed to read {dir}; make sure it exists"));
+    OPML::from_str(&file).expect(&format!("Failed to read {dir}; make sure it is a valid opml file"))
+}
