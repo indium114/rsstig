6511c9492d8119c7fb0a587fdfd1aa9a26168b0b
indium114 <156162907+indium114@users.noreply.github.com>
2026-09-04 15:17:33 +0200

    feat: print info about each feed

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,10 @@ mod opml;
 fn main() -> Result<()> {
     color_eyre::install()?;
 
-    println!("{:#?}", opml::load_feeds());
+    for feed in opml::load_feeds().body.outlines {
+        println!("Feed: {}", feed.text);
+        println!("  > Description : {}", feed.description.unwrap_or("<no description>".to_string()).trim());
+    }
 
     Ok(())
 }
