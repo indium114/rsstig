c6d0937878aa11fbdf1ecabaadee7722ae6404b9
indium114 <156162907+indium114@users.noreply.github.com>
2026-09-04 16:40:11 +0200

    feat: add progress bar while feeds are downloaded

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use color_eyre::eyre::Result;
+use indicatif::{ProgressBar, ProgressStyle, ProgressState};
+use std::fmt::Write;
 
 mod opml;
 mod rss;
@@ -12,7 +14,15 @@ fn main() -> Result<()> {
     color_eyre::install()?;
 
     let mut feeds: Vec<Feed> = Vec::new();
-    for feed in opml::load_feeds() {
+    let feed_files = opml::load_feeds();
+
+    let bar = ProgressBar::new(feed_files.len() as u64);
+    bar.set_style(ProgressStyle::with_template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.white}] ({eta})")
+        .unwrap()
+        .with_key("eta", |state: &ProgressState, w: &mut dyn Write| write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap())
+        .progress_chars("#>-"));
+
+    for feed in feed_files {
         feeds.push(Feed {
             name: feed.text.clone(),
             entries: rss::get_rss(feed.xml_url.unwrap_or_else(|| {
@@ -21,8 +31,12 @@ fn main() -> Result<()> {
                     format!("Feed {} does not have an xmlUrl", feed.text).to_string()
                 )
             })),
-        })
+        });
+
+        bar.inc(1);
     }
+
+    bar.finish();
 
     Ok(())
 }
